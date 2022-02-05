442b198f3fa578ddf375089c4fc73e054d8ce90a
Jonathan Florido-Carait <jonathan.carait@outlook.com>
2022-02-05 15:51:06 +1100

    adding renderlicence badge function

--- README.md
+++ README.md
@@ -1,11 +1,35 @@
-# test
+# asdf
 
-  undefined
-  
-  ## Installation Instructions: 
-  
-  undefined
-  
-  ## Usage Information: 
-  
-  undefined
+## Description
+
+asdf
+
+## Table of contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Contribute]](#How to contribute)
+- [Tests](#Tests)
+- [Questions](#Questions)
+
+## Installation Instructions
+
+sdf
+
+## Usage Information:
+
+fdsarge
+
+## License
+
+Apache 2.0
+
+## How to contribute
+erewrew
+
+## Tests
+rew
+
+### Questions
--- js/utils/generateMarkdown.js
+++ js/utils/generateMarkdown.js
@@ -1,6 +1,19 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+'MIT', 'GNU GPLv3', 'Boost 1.0', 'Unlicense'
+
+function renderLicenseBadge(license) {
+    
+  switch (license) {
+    case 'Apache 2.0':
+      return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    case 'MIT':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    case ''
+  }
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -15,15 +28,40 @@ function generateMarkdown(data) {
 
   return `# ${data.title}
 
-  ${data.description}
-  
-  ## Installation Instructions: 
-  
-  ${data.installation}
-  
-  ## Usage Information: 
-  
-  ${data.usage}`;
+## Description
+
+${data.description}
+
+## Table of contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Contribute]](#How to contribute)
+- [Tests](#Tests)
+- [Questions](#Questions)
+
+## Installation Instructions
+
+${data.installation}
+
+## Usage Information:
+
+${data.usage}
+
+## License
+
+${data.license}
+
+## How to contribute
+${data.contribute}
+
+## Tests
+${data.tests}
+
+### Questions
+${data.questions}`;
 };
 
 module.exports = generateMarkdown;
