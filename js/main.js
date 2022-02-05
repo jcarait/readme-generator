ca41cd3b3ff33dc1f329f024d3ff0231d77378de
Jonathan Florido-Carait <jonathan.carait@outlook.com>
2022-02-05 15:38:55 +1100

    added additional prompts to inquirer

--- js/main.js
+++ js/main.js
@@ -16,35 +16,54 @@ function promptUser() {
       name: 'title',
       message: 'What is the title of your project?',
     },
-    // {
-    //   type: 'input',
-    //   name: 'description',
-    //   message: 'Enter a short description of your project'
-    // },
-    // {
-    //   type: 'input',
-    //   name: 'installation',
-    //   message: 'What installation is required to run your project?'
-    // },
-    // {
-    //   type: 'input',
-    //   name: 'usage',
-    //   message: 'How do you use your application?'
-    // },
-    // {
-    //   type: 'list',
-    //   message: 'How would you like to licence your project?',
-    //   name: 'license',
-    //   choices: ['Apache 2.0', 'MIT', 'GNU GPLv3', 'Boost 1.0', 'Unlicense']
-    // }
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter a short description of your project'
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What installation is required to run your project?'
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'How do you use your application?'
+    },
+    {
+      type: 'list',
+      message: 'Choose a license your project.',
+      name: 'license',
+      choices: ['Apache 2.0', 'MIT', 'GNU GPLv3', 'Boost 1.0', 'Unlicense']
+    },
+    {
+      type: 'input',
+      name: 'contribute',
+      message: 'Enter guidelines on how to contribute to your project.'
+    },
+    {
+      type: 'input',
+      name: 'contribute',
+      message: 'Enter guidelines on how to contribute to your project.'
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'How do you test your application?'
+    },
+    {
+      type: 'input',
+      name: 'questions',
+      message: 'Provide instructions on how to reach you with additional questions'
+    },
   ]);
 }
-
-//Variable is populated with answers from Inquirer
 
 const init = () => {
   promptUser()
   .then((answers) => {
+    console.log(answers);
     const readmeContents = markdown(answers);
 
     fs.writeFileSync('../README.md', readmeContents)
